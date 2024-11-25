e5b1f1c16b57439e8d48163a03931f5600eb9ef7
Andrey Dantas <andreydantasvf@gmail.com>
2024-11-25 20:57:34 -0300

    feat: create first storybook doc

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,11 @@
-export const Main = () => {
+export const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJs, NextJs'
+}) => {
   return (
-    <main>
-      <h1>React Avançado</h1>
+    <main className="bg-zinc-900 p-2 rounded-2xl flex flex-col gap-1 items-center justify-center w-80 h-80 m-auto">
+      <h1 className="text-slate-200 text-4xl">{title}</h1>
+      <p className="text-slate-500">{description}</p>
     </main>
   );
 };
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -4,7 +4,16 @@ import { Main } from '.';
 
 export default {
   title: 'Main',
-  component: Main
+  component: Main,
+  parameters: {
+    layout: 'fullscreen'
+  }
 } as Meta;
 
 export const Default: StoryObj = {};
+export const Basic: StoryObj = {
+  args: {
+    title: 'React Basic',
+    description: 'Description Basic'
+  }
+};
