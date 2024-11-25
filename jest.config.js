d58bc5f076fcd8503862ad496c46d6c368cea99b
Andrey Dantas <andreydantasvf@gmail.com>
2024-11-25 20:58:07 -0300

    feat: ignore stories.tsx in jest and update snapshot

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,8 @@ module.exports = {
   collectCoverageFrom: [
     'src/**/*.ts(x)?',
     '!src/app/**', // should be tested in e2e
-    '!src/types/**'
+    '!src/types/**',
+    '!src/**/stories.tsx'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
